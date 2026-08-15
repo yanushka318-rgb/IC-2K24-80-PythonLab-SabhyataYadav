1bb34ba20aa5b9921999d4964e5c443660baf881
Sabhyata Yadav <yanushka318@gmail.com>
2026-08-15 17:12:15 +0530

    Update celsius_to_farenheit.py

---
+++ lab1/celsius_to_farenheit.py
@@ -0,0 +1,8 @@
+# This program takes temperature in Celsius as input
+# and converts it into Fahrenheit.
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
