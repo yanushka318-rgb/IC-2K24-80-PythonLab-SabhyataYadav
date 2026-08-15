9a790685e8ed014447217ccfd7c42109bd1e01d0
Sabhyata Yadav <yanushka318@gmail.com>
2026-08-15 17:14:29 +0530

    Update escape_sequence.py

---
+++ lab1/escape_sequence.py
@@ -0,0 +1,13 @@
+# This program prints a small receipt using \t and \n
+# to arrange the items and prices neatly.
+
+print("        SHOPPING RECEIPT")
+print("-----------------------------")
+print("Item\t\tPrice")
+print("-----------------------------")
+print("Pen\t\t₹20")
+print("Notebook\t₹50")
+print("Pencil\t\t₹10")
+print("Eraser\t\t₹5")
+print("-----------------------------")
+print("Total\t\t₹85")
